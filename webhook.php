3ffb73ebd1f3701783d742e694d9d359ba687792
PersiLiao <xiangchu.liao@gmail.com>
2021-01-19 10:37:02 +0800

    fixed event error

--- webhook.php
+++ webhook.php
@@ -69,7 +69,7 @@ try{
             foreach($repositoryConfig['command'] as $command){
                 if((!isset($command['branch']) || empty($command['branch'])) || (isset($command['branch']) &&
                         $command['branch'] === $branchName)){
-                    if((isset($command['event']) && !empty($command['event'])) && (isset($command['exec']) && !empty
+                    if((isset($command['event']) && !empty($command['event']) && $command['event'] === 'push') && (isset($command['exec']) && !empty
                             ($command['exec']))){
                         if(is_array($command['exec'])){
                             foreach($command['exec'] as $exec){
